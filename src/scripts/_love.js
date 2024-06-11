f6a240415000ea6341f091066796ee50d1db27b0
Anton Shchukin <a.a.shchukin@gmail.com>
2024-06-11 21:00:01 +0700

    swiper 2 columns

--- src/scripts/_love.js
+++ src/scripts/_love.js
@@ -1,31 +1,49 @@
-let swiperInstance;
+// let swiperInstance;
+//
+// function initializeSwiper() {
+//     if (window.matchMedia('(min-width: 741px)').matches) {
+//         swiperInstance = new Swiper('.love .swiper', {
+//             autoHeight: true,
+//             slidesPerView: 2,
+//             grid: {
+//                 rows: 2,
+//             },
+//             spaceBetween: 40,
+//             mousewheel: {
+//                 forceToAxis: true,
+//             },
+//             pagination: {
+//                 el: '.swiper-pagination',
+//                 clickable: true,
+//             },
+//         });
+//     }
+// }
+//
+// // Initialize on page load
+// initializeSwiper();
+//
+// // Re-initialize on window resize
+// window.addEventListener('resize', function() {
+//
+//     // Clear existing Swiper instance if it exists
+//     if (swiperInstance) {
+//         swiperInstance.destroy(true, true);
+//         swiperInstance = null;
+//     }
+//
+//     initializeSwiper();
+// });
 
-function initializeSwiper() {
-    if (window.matchMedia('(min-width: 741px)').matches) {
-        swiperInstance = new Swiper('.love .swiper', {
-            spaceBetween: 40,
-            mousewheel: {
-                forceToAxis: true,
-            },
-            pagination: {
-                el: '.swiper-pagination',
-                clickable: true,
-            },
-        });
-    }
-}
 
-// Initialize on page load
-initializeSwiper();
-
-// Re-initialize on window resize
-window.addEventListener('resize', function() {
-
-    // Clear existing Swiper instance if it exists
-    if (swiperInstance) {
-        swiperInstance.destroy(true, true);
-        swiperInstance = null;
-    }
-
-    initializeSwiper();
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 3,
+    grid: {
+        rows: 2,
+    },
+    spaceBetween: 30,
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
 });
