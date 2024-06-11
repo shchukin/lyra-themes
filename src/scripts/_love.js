467f828b4f8aaee4ef6ad5fc0d177832acead896
Anton Shchukin <a.a.shchukin@gmail.com>
2024-06-11 21:49:13 +0700

    re-init slider

--- src/scripts/_love.js
+++ src/scripts/_love.js
@@ -1,34 +1,54 @@
-new Swiper('.love .swiper', {
-    breakpoints: {
-        // when window width is >= 320px
-        320: {
-            slidesPerView: 1,
-            slidesPerGroup: 1,
-            spaceBetween: getComputedStyle(document.documentElement).getPropertyValue('--container-padding'),
-            scrollbar: {
-                el: '.swiper-scrollbar',
+let loveSwiperInstance;
+let resizeTimeout;
+
+function initSwiper() {
+    loveSwiperInstance = new Swiper('.love .swiper', {
+        breakpoints: {
+            // when window width is >= 320px
+            320: {
+                slidesPerView: 1,
+                slidesPerGroup: 1,
+                spaceBetween: parseInt(getComputedStyle(document.documentElement).getPropertyValue('--container-padding')),
+                scrollbar: {
+                    el: '.swiper-scrollbar',
+                },
+                pagination: false
             },
-            pagination: false
+            // when window width is >= 741px
+            741: {
+                slidesPerView: 2,
+                slidesPerGroup: 2,
+                spaceBetween: 40,
+                grid: {
+                    rows: 2,
+                },
+                scrollbar: false,
+                pagination: {
+                    el: '.swiper-pagination',
+                    clickable: true,
+                },
+            },
         },
-        // when window width is >= 741px
-        741: {
-            slidesPerView: 2,
-            slidesPerGroup: 2,
-            spaceBetween: 40,
-            grid: {
-                rows: 2,
-            },
-            scrollbar: false,
-            pagination: {
-                el: '.swiper-pagination',
-                clickable: true,
-            },
+        autoHeight: true,
+        mousewheel: {
+            forceToAxis: true,
         },
-    },
-    autoHeight: true,
-    mousewheel: {
-        forceToAxis: true,
-    },
-});
+    });
+}
+
+// Init Swiper on page load
+initSwiper();
 
 
+// Init Swiper on window resize
+function handleResize() {
+    clearTimeout(resizeTimeout);
+    resizeTimeout = setTimeout(() => {
+        if (loveSwiperInstance) {
+            loveSwiperInstance.destroy(true, true);
+        }
+        initSwiper();
+    }, 1000); // 2000ms delay
+}
+
+window.addEventListener('resize', handleResize);
