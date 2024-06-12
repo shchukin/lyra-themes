1a9995eb2de8aa3cbdef224bad9e76c87519beb8
Anton Shchukin <a.a.shchukin@gmail.com>
2024-06-12 22:49:30 +0700

    header burger

---
+++ src/scripts/_header.js
@@ -0,0 +1,21 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const $burger = document.querySelector('.header__toggle-nav');
+    const $nav = document.querySelector('.header__nav');
+
+    $burger.addEventListener('click', function (event) {
+        event.stopPropagation();
+        $nav.classList.toggle('header__nav--expanded');
+    });
+
+    document.addEventListener('keydown', function (event) {
+        if (event.key === 'Escape') {
+            $nav.classList.remove('header__nav--expanded');
+        }
+    });
+
+    document.addEventListener('click', function (event) {
+        if (!$nav.contains(event.target) && !$burger.contains(event.target)) {
+            $nav.classList.remove('header__nav--expanded');
+        }
+    });
+});
